5a8bf9928b77017e3b8d9a81513a9ee9cc9dae40
vduseev <vagiz.d@gmail.com>
2017-09-26 13:33:17 +0200

    Added description to bash script draft

--- es-setup.sh
+++ es-setup.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
+# Pure bash script draft to set up a whole installment
+# without accessing the GUI
 
 DOMAIN="smart-enron"
 ES_VERSION=5.5
-INSTANCE_COUNT=2
+INSTANCE_COUNT=1
 VOLUME_SIZE=10
 IAM_ARN="arn:aws:iam::610780818249:root"
 RES_ARN="arn:aws:es:us-east-1:610780818249:domain/"$DOMAIN"/*"
+PROXY_IP=54.89.200.81
 
 
 aws es create-elasticsearch-domain \
@@ -25,10 +28,26 @@ aws es update-elasticsearch-domain-config \
             {
               "Effect": "Allow",
               "Principal": {
-                "AWS":' $IAM_ARN '
+                "AWS": ' $IAM_ARN '
               },
               "Action": "es:*",
-              "Resource":' $RES_ARN '
+              "Resource": ' $RES_ARN '
+            },
+            {
+              "Sid": "",
+              "Effect": "Allow",
+              "Principal": {
+                "AWS": "*"
+              },
+              "Action": "es:*",
+              "Resource": ' $RES_ARN '
+              "Condition": {
+                "IpAddress": {
+                  "aws:SourceIp": ' $PROXY_IP '
+                }
+              }
             }
           ]
         }'
+
+# Setup EC2 proxy fir Kibana
